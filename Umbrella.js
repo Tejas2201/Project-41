628260c3817eb5ec76c10b36afc8d77f3ce4e3e0
TJ <77736421+Tejas2201@users.noreply.github.com>
2022-05-26 12:56:07 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,19 @@
+class Umbrella {
+    constructor(x, y) {
+        var options = {
+            isStatic:true
+        }
+        this.image = loadImage("images/Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x, y, 50, options);
+        
+        World.add(world, this.umbrella);
+    }
+    display() {
+
+        var pos = this.umbrella.position;
+
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,300,300);
+    }
+
+};
